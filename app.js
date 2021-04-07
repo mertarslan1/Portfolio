1f1359e53f8e1221cada055b3fc36f116df0883f
mertarslan1 <73879835+mertarslan1@users.noreply.github.com>
2021-04-07 13:06:54 +0200

    Finetuning

---
+++ app.js
@@ -0,0 +1,25 @@
+//menu
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+// text
+var typed = new Typed(".animate", {
+  strings: ["student", "designer", "gamer", "freelancer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
+
+//aos
+AOS.init();
+
